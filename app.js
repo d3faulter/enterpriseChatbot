2d0d5cf0d3edf050064a3df5596f49f63abe80cb
d3faulter <nvfbeck@gmail.com>
2023-07-12 11:53:56 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const { ActivityHandler, MessageFactory } = require('botbuilder');
+
+class EchoBot extends ActivityHandler {
+    constructor() {
+        super();
+        this.onMessage(async (context, next) => {
+            const replyText = `Echo: ${ context.activity.text }`;
+            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            await next();
+        });
+
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity('Hello and welcome!');
+                }
+            }
+            await next();
+        });
+    }
+}
+
+module.exports.EchoBot = EchoBot;
